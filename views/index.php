092950096fc7b293d4dd263716d0810be66ed983
Hugo Sundberg <55212646+Hugocsundberg@users.noreply.github.com>
2020-12-27 22:40:02 +0100

    first implementation of start page

--- views/index.php
+++ views/index.php
@@ -1,7 +1,22 @@
 <?php require('header.php') ?>
 
 <body>
-    <a href="views/login.php">Login</a>
+    <a href="/views/login.php">Login</a>
+
+    <div class="post">
+        <div class="date-section"></div>
+        <div class="image-section">
+            <img src="/images/photo-1609050470947-f35aa6071497.jpeg" alt="">
+        </div>
+        <div class="text-section">
+            <h2>Hello post headline</h2>
+            <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Voluptatibus inventore expedita pariatur eius facilis, saepe ex quae ab atque p</p>
+        </div>
+        <div class="bottom-section">
+            <p></p>
+        </div>
+
+    </div>
     <script src="./script.js"></script>
 </body>
 

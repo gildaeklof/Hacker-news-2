6801073c0c2291c606ad00d0e4d9e1815c205082
Hugo Sundberg <55212646+Hugocsundberg@users.noreply.github.com>
2020-12-28 19:01:52 +0100

    Update landing page

--- views/index.php
+++ views/index.php
@@ -1,22 +1,77 @@
-<?php require('header.php') ?>
+<?php
+require('header.php');
+require('../functions.php');
+logMessage();
+
+//Database connection
+$db = new PDO('sqlite:../hacker_news_database.sqlite3');
+
+//Fetch posts from database
+$result = $db->query("SELECT * FROM Posts");
+$posts = $result->fetchAll(PDO::FETCH_ASSOC);
+?>
 
 <body>
     <a href="/views/login.php">Login</a>
 
-    <div class="post">
-        <div class="date-section"></div>
-        <div class="image-section">
-            <img src="/images/photo-1609050470947-f35aa6071497.jpeg" alt="">
-        </div>
-        <div class="text-section">
-            <h2>Hello post headline</h2>
-            <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Voluptatibus inventore expedita pariatur eius facilis, saepe ex quae ab atque p</p>
-        </div>
-        <div class="bottom-section">
-            <p></p>
-        </div>
+    <?php foreach ($posts as $post) : ?>
+        <?php
 
-    </div>
+        $userId = $post['user_id'];
+
+        //Fetch users from database
+        $result = $db->query("SELECT * FROM Users WHERE id = $userId");
+        $user = $result->fetch(PDO::FETCH_ASSOC);
+
+        //If user has a name, set it to $userName
+        if (isset($user['name'])) {
+            $userName = $user['name'];
+        } else {
+            $userName = 'IHaveNoName';
+        }
+
+        ?>
+        <div class="post">
+            <div class="date-section">
+                <div class="left">
+                    <img src="/images/photo-1609050470947-f35aa6071497.jpeg" alt="">
+                    <p class="name"><?= $userName ?></p>
+                </div>
+                <div class="right">
+                    <p class="date"><?= date('D M Y H:i', $post['date']) ?></p>
+                </div>
+            </div>
+            <div class="image-section">
+                <img src="/images/photo-1609050470947-f35aa6071497.jpeg" alt="">
+            </div>
+            <div class="text-section">
+                <div class="text-section-text">
+                    <h2><?= $post['header'] ?></h2>
+                    <p><?= $post['body'] ?></p>
+                </div>
+                <div class="text-section-vote">
+                    <div class="img-container">
+                        <img class="upvote" src="/assets/up-arrow.svg" alt="">
+                    </div>
+                    <p>42</p>
+                    <div class="img-container">
+                        <img class="downvote" src="/assets/down-arrow.svg" alt="">
+                    </div>
+                </div>
+            </div>
+            <div class="bottom-section">
+                <div class="left">
+                    <button>comment</button>
+                </div>
+                <?php if (isset($_SESSION['user']) && $post['user_id'] === $_SESSION['user']['id']) : ?>
+                    <div class="right">
+                        <button class="edit-button">Edit</button>
+                        <button class="delete-button">Delete</button>
+                    </div>
+                <?php endif ?>
+            </div>
+        </div>
+    <?php endforeach ?>
     <script src="./script.js"></script>
 </body>
 

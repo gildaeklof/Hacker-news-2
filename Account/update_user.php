7da6659cb08702b009f0f9d85c696c9f3a45da5e
Hugo Sundberg <55212646+Hugocsundberg@users.noreply.github.com>
2021-01-03 19:11:35 +0100

    Add image upload support

--- Account/update_user.php
+++ Account/update_user.php
@@ -2,18 +2,42 @@
 
 declare(strict_types=1);
 require('../functions.php');
+$user_id = $_SESSION['user']['id'];
 
-//Vars
+//Image
+if (isset($_FILES['file'])) {
+
+    //File proporties
+    $file_name = $_FILES['file']['name'];
+    $file_tmp = $_FILES['file']['tmp_name'];
+    $file_size = $_FILES['file']['size'];
+    $file_error = $_FILES['file']['error'];
+    $fileNameExploded = explode('.', $file_name);
+    $file_extention = end($fileNameExploded);
+    $file_name_new = "profile_picture" . $user_id . '.' . $file_extention;
+    $file_destination = __DIR__ . '/uploads/' . $file_name_new;
+    $file_relative_path = '/Account/uploads/' . $file_name_new;
+
+    move_uploaded_file($file_tmp, $file_destination);
+}
+
+// Other
 $name = $_POST['name'];
 $email = $_POST['email'];
 $password_hash = password_hash($_POST['password'], PASSWORD_BCRYPT);
 $bio = $_POST['bio'];
 $user_id = $_SESSION['user']['id'];
 
-print_r($_POST);
-
 //db connection
 $db = new PDO('sqlite:../hacker_news_database.sqlite3');
+
+//Add avatar path to db
+$stmt = $db->prepare("UPDATE Users SET avatar_path = :file_destination WHERE id = :user_id");
+$stmt->bindParam(':file_destination', $file_relative_path);
+$stmt->bindParam(':user_id', $user_id);
+$stmt->execute();
+
+//Add other to db
 $stmt = $db->prepare("UPDATE Users SET name = :name, email = :email, password_hash = :password_hash, bio = :bio WHERE id = :user_id;");
 $stmt->bindParam(':name', $name);
 $stmt->bindParam(':email', $email);
--- views/account.php
+++ views/account.php
@@ -2,22 +2,32 @@
 
 require('header.php');
 require('../functions.php');
-if (!$_SESSION['user']) {
-    redirect('/views/login.php');
-}
 
 //Database connection
 $db = new PDO('sqlite:../hacker_news_database.sqlite3');
+
+if (!$_SESSION['user']) {
+    redirect('/views/login.php');
+} else {
+    $result = $db->query("SELECT * FROM USERS WHERE id = 11");
+    $userData = $result->fetch(PDO::FETCH_ASSOC);
+    $_SESSION['user'] = $userData;
+}
+
+
+
 ?>
 
 <body>
     <section class="top">
         <h1>Account</h1>
-        <img src="" alt="" class="profile-image">
-        <button class="edit-image"></button>
+        <img src="<?= $_SESSION['user']['avatar_path'] ?>" alt="" class="profile-image">
     </section>
     <section class="form">
-        <form action="/Account/update_user.php" method="POST">
+        <form action="/Account/update_user.php" method="post" enctype="multipart/form-data">
+
+            <label for="file">Image</label>
+            <input type="file" name="file" id="file">
             <label for="email">Email</label>
             <input type="text" name="email" id="email">
             <label for="name">Name</label>
